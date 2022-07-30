ddb906672d336d386a9aa381f7b6c39e960a8c34
styrbjorne <53510825+styrbjorne@users.noreply.github.com>
2022-07-29 22:56:14 -0700

    first commit

---
+++ assets/scripts/app.js
@@ -0,0 +1,77 @@
+class Product {
+  // title = 'DEFAULT';
+  // imageUrl;
+  // description;
+  // price;
+
+  constructor(title, image, desc, price) {
+    this.title = title;
+    this.imageUrl = image;
+    this.description = desc;
+    this.price = price;
+  }
+}
+
+class ProductItem {
+  constructor(product) {
+    this.product = product;
+  }
+
+  addToCart() {
+    console.log('Adding product to cart...');
+    console.log(this.product);
+  }
+
+  render() {
+    const prodEl = document.createElement('li');
+    prodEl.className = 'product-item';
+    prodEl.innerHTML = `
+        <div>
+          <img src="${this.product.imageUrl}" alt="${this.product.title}" >
+          <div class="product-item__content">
+            <h2>${this.product.title}</h2>
+            <h3>\$${this.product.price}</h3>
+            <p>${this.product.description}</p>
+            <button>Add to Cart</button>
+          </div>
+        </div>
+      `;
+    const addCartButton = prodEl.querySelector('button');
+    addCartButton.addEventListener('click', this.addToCart);
+    return prodEl;
+  }
+}
+
+class ProductList {
+  products = [
+    new Product(
+      'A Pillow',
+      'https://content.instructables.com/ORIG/F7D/WJKO/I74N5P2F/F7DWJKOI74N5P2F.jpg?auto=webp&frame=1',
+      'A soft pillow!',
+      '19.99'
+    ),
+    new Product(
+      'A Carpet',
+      'https://architecturesideas.com/wp-content/uploads/2017/11/carpet-designs-13.jpg',
+      'A carpet which you might like - or not.',
+      '89.99'
+    ),
+  ];
+
+  constructor() {}
+
+  render() {
+    const renderHook = document.getElementById('app');
+    const prodList = document.createElement('ul');
+    prodList.className = 'product-list';
+    for (const prod of this.products) {
+      const productItem = new ProductItem(prod);
+      const prodEl = productItem.render();
+      prodList.append(prodEl);
+    }
+    renderHook.append(prodList);
+  }
+}
+
+const productList = new ProductList();
+productList.render();
